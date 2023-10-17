c59975f293943115e94892c662e8ba415f27a50c
miky211 <142940411+migue213@users.noreply.github.com>
2023-10-17 17:19:27 -0400

    version 2023-08-29_01

---
+++ juegos.py
@@ -0,0 +1,21 @@
+def escoger_juego():
+    import ahorcado
+    import adivinanza
+
+    print('================================')
+    print('Elija su Juego')
+    print('================================')
+
+    print("(1) Ahorcado (2) Adivinanza")
+
+    juego = int(input("¿Cuál juego? "))
+
+    if (juego == 1):
+        print("Jugando ahorcado")
+        ahorcado.jugar()
+    elif (juego == 2):
+        print("Jugando adivinanza")
+        adivinanza.jugar()
+        
+if(__name__ == "__main__"):
+    escoger_juego()
